8b9779e13f7288ee9918ef9fadadca08c665004c
SJU Sustainability Office <sjusustainoffice@csbsju.edu>
2013-10-13 21:55:04 -0500

    Added rudimentary error handling to archiveVersion
    ... for when tidy doesn't work

--- archiveVersion.py
+++ archiveVersion.py
@@ -17,89 +17,89 @@ from configobj import ConfigObj
 import errno
 import shutil
 
-# TODO:
-# - Create publish option
-#		- Copy files to web location (before sending to Premailer)
-#		- Set proper permissions
-#		- Change src attributes to the new locations via Premailer
+# TODO: Improve error handling for when Tidy doesn't work.
 
 def FileExists(filename):
-	try:
-		fp = open(filename)
-	except IOError as e:
-		if e.errno == errno.ENOENT: # If the file does not exist
-			print "Warning: {0} does not exist".format(filename)
-			return False
-		elif e.errno == errno.EACCES: # If you don't have permission
-			print "Warning: Permission denied to access {0}".format(filename)
-		else:
-			raise
-	else:
-		fp.close()
-		return True
+    try:
+        fp = open(filename)
+    except IOError as e:
+        if e.errno == errno.ENOENT: # If the file does not exist
+            print "Warning: {0} does not exist".format(filename)
+            return False
+        elif e.errno == errno.EACCES: # If you don't have permission
+            print "Warning: Permission denied to access {0}".format(filename)
+        else:
+            raise
+    else:
+        fp.close()
+        return True
 
 def AppendFilename(filename, appendix):
-	splitFilename = splitext(filename)
-	return splitFilename[0] + appendix + splitFilename[1]
+    splitFilename = splitext(filename)
+    return splitFilename[0] + appendix + splitFilename[1]
 
 def TidyHTML(fileAsString, optDict):
-	optDictList = []
-	for key, value in optDict.iteritems():
-		optDictList.append(key)
-		if value != '':
-			optDictList.append(value)
+    optDictList = []
+    for key, value in optDict.iteritems():
+        optDictList.append(key)
+        if value != '':
+            optDictList.append(value)
 
-	tidy = subprocess.Popen(['tidy'] + optDictList,
-													stdin=subprocess.PIPE,
-													stdout=subprocess.PIPE,
-													stderr=subprocess.PIPE)
-	results = tidy.communicate(fileAsString)[0]
-	# Remove CDATA tags and return
-	return results.replace('/*<![CDATA[*/\n', '').replace('/*]]>*/\n','')
+    tidy = subprocess.Popen(['tidy'] + optDictList,
+                                                    stdin=subprocess.PIPE,
+                                                    stdout=subprocess.PIPE,
+                                                    stderr=subprocess.PIPE)
+    results = tidy.communicate(fileAsString)[0]
+    # Remove CDATA tags and return
+    # TODO: Improve error handling for when Tidy doesn't work.
+    if (results != ''):
+        return results.replace('/*<![CDATA[*/\n', '').replace('/*]]>*/\n','')
+    else:
+        print "There's a problem with tidy."
 
 def CleanDirPath(path):
-	""" Return a directory path that is terminated by '/'.
-	"""
-	def CleanPath(path):
-		if (os.path.split(path)[1] != ''):
-			return path + '/'
-		else:
-			return path
-	
-	if urlparse(path).scheme == '': # This means that the path is local
-		return CleanPath(path)
-	else:
-		parsedURL = urlparse(path)
-		return urlunparse(ParseResult(
-			parsedURL.scheme,
-			parsedURL.netloc,
-			CleanPath(parsedURL.path),
-			'', '', ''
-		))
+    """ Return a directory path that is terminated by '/'.
+    """
+    def CleanPath(path):
+        if (os.path.split(path)[1] != ''):
+            return path + '/'
+        else:
+            return path
+    
+    if urlparse(path).scheme == '': # This means that the path is local
+        return CleanPath(path)
+    else:
+        parsedURL = urlparse(path)
+        return urlunparse(ParseResult(
+            parsedURL.scheme,
+            parsedURL.netloc,
+            CleanPath(parsedURL.path),
+            '', '', ''
+        ))
 
 def PathWalk(basePath, targetPath):
-	for dirpath, dirnames, filenames in os.walk(basePath):
-		if targetPath.startswith(dirpath):
-			yield dirpath, dirnames, filenames
+    for dirpath, dirnames, filenames in os.walk(basePath):
+        if targetPath.startswith(dirpath):
+            yield dirpath, dirnames, filenames
 
 def WebDirPerms(basePath, targetPath):
-	for dirpath, dirnames, filenames in \
-			PathWalk(basePath, targetPath):
-		os.chmod(dirpath, 0755)
+    for dirpath, dirnames, filenames in \
+            PathWalk(basePath, targetPath):
+        os.chmod(dirpath, 0755)
 
 
 # Create a dictionary to hold tidy options:
 tidyOptions = {
-	'-i': '',
-	'--tab-size': '2',
-	'--wrap': '0',
-	'--tidy-mark': 'n',
-	'--merge-divs': 'n',
-	'--doctype': 'strict',
-	'--char-encoding': 'utf8',
-	'--hide-comments': 'n',
-	'--drop-proprietary-attributes': 'y',
-	'--preserve-entities': 'y'
+    '-i': '',
+    '--tab-size': '2',
+    '--wrap': '0',
+    '--tidy-mark': 'n',
+    '--merge-divs': 'n',
+    '--doctype': 'strict',
+    '--char-encoding': 'utf8',
+    '--hide-comments': 'n',
+    '--drop-proprietary-attributes': 'y',
+    '--preserve-entities': 'y'
 }
 
 # Create a dictionary for all the filenames
@@ -126,12 +126,12 @@ urls = {}
 
 # Set up the URL for the Archive page and images
 if settings.as_bool('publish_files') == True:
-	archivePath = urljoin(settings['web_path_root'], settings['path_to_site_folder'])
-	urls['Archive'] = urljoin(archivePath, filenames['Archive'])
-	urls['Base'] = urljoin(settings['web_path_root'], settings['path_to_site_folder'])
+    archivePath = urljoin(settings['web_path_root'], settings['path_to_site_folder'])
+    urls['Archive'] = urljoin(archivePath, filenames['Archive'])
+    urls['Base'] = urljoin(settings['web_path_root'], settings['path_to_site_folder'])
 else:
-	urls['Archive'] = "./" + filenames['Archive']
-	urls['Base'] = "./"
+    urls['Archive'] = "./" + filenames['Archive']
+    urls['Base'] = "./"
 
 # Set up the URL for the images
 
@@ -179,30 +179,30 @@ soup.body.insert(len(soup.body.contents), trackingTag)
 filesToPublish = []
 print "Adding image dimensions to all <img /> tags..."
 for img in soup.findAll('img'):
-	imgURL = img['src']
-	# urlopen doesn't work with relative urls, so we have to test to see if imgURL is relative or not
-	if urlparse(imgURL).scheme == '': # This means that the image URL is relative (and local)
-		if FileExists(imgURL):
-			dimens = Image.open(imgURL).size
-			filesToPublish.append(imgURL)
+    imgURL = img['src']
+    # urlopen doesn't work with relative urls, so we have to test to see if imgURL is relative or not
+    if urlparse(imgURL).scheme == '': # This means that the image URL is relative (and local)
+        if FileExists(imgURL):
+            dimens = Image.open(imgURL).size
+            filesToPublish.append(imgURL)
 
-	else:
-		imgFile = cStringIO.StringIO(urllib2.urlopen(imgURL).read())
-		dimens = Image.open(imgFile).size
+    else:
+        imgFile = cStringIO.StringIO(urllib2.urlopen(imgURL).read())
+        dimens = Image.open(imgFile).size
 
-	# set image width and height
-	img['width'] = dimens[0]
-	img['height'] = dimens[1]
+    # set image width and height
+    img['width'] = dimens[0]
+    img['height'] = dimens[1]
 
 # Find all a tags in the soup. If the link is local, add the href to
 # a list so we know which files to publish.
 print "Finding other files to publish..."
 for a in soup.findAll('a'):
-	aURL = a['href']
-	if urlparse(aURL).scheme == '':
-		if FileExists(aURL):
-			filesToPublish.append(aURL)
-	
+    aURL = a['href']
+    if urlparse(aURL).scheme == '':
+        if FileExists(aURL):
+            filesToPublish.append(aURL)
+    
 # Turn the soup back into a string
 print 'Reformatting original HTML file...'
 files['Original'] = str(soup)
@@ -216,111 +216,111 @@ files['Archive'].write(TidyHTML(archiveVersion, tidyOptions))
 
 archiveSoup = BeautifulSoup(m.group(0))
 for a in archiveSoup.findAll('a'):
-			a['href'] = urls['Archive']
+            a['href'] = urls['Archive']
 
 files['Original'] = TidyHTML(files['Original'][:m.start(0)] + \
-										str(archiveSoup) + \
-										files['Original'][m.end(0):], \
-										tidyOptions)
+                                        str(archiveSoup) + \
+                                        files['Original'][m.end(0):], \
+                                        tidyOptions)
 
 # Close the files so that we can read some of them
 for value in files.itervalues():
-	if isinstance(value, file): # If it's a file object, close it
-		value.close()
+    if isinstance(value, file): # If it's a file object, close it
+        value.close()
 
 # Create the content to send to Premailer
-	content = files['Original']
+    content = files['Original']
 
 if settings.as_bool('use_premailer') == True:
-		# Premailer will put xmlns="http://www.w3.org/1999/xhtml" into the html tag no matter
-	# if it's already there or not, so remove it from the content we're sending.
-	#content = content.replace(' xmlns="http://www.w3.org/1999/xhtml"', '')
+        # Premailer will put xmlns="http://www.w3.org/1999/xhtml" into the html tag no matter
+    # if it's already there or not, so remove it from the content we're sending.
+    #content = content.replace(' xmlns="http://www.w3.org/1999/xhtml"', '')
 
-	# Now, send the email version to Premailer:
-	print "Sending to Premailer..."
-	urls['Premailer'] = "http://premailer.dialect.ca/api/0.1/documents"
-	parameterDict = {'html': content, 'remove_comments': 'true', 
-										'base_url': urls['Base']}
-	if settings.as_bool('publish_files') == False:
-		del parameterDict['base_url']
-	params = urllib.urlencode(parameterDict)
-	conn = httplib.HTTPConnection("premailer.dialect.ca")
-	conn.request("POST", urls['Premailer'], params)
-	response = conn.getresponse()
-	print "Response from Premailer received with status:",
-	print response.status, response.reason
-	premailerData = response.read()
-	conn.close()
+    # Now, send the email version to Premailer:
+    print "Sending to Premailer..."
+    urls['Premailer'] = "http://premailer.dialect.ca/api/0.1/documents"
+    parameterDict = {'html': content, 'remove_comments': 'true', 
+                                        'base_url': urls['Base']}
+    if settings.as_bool('publish_files') == False:
+        del parameterDict['base_url']
+    params = urllib.urlencode(parameterDict)
+    conn = httplib.HTTPConnection("premailer.dialect.ca")
+    conn.request("POST", urls['Premailer'], params)
+    response = conn.getresponse()
+    print "Response from Premailer received with status:",
+    print response.status, response.reason
+    premailerData = response.read()
+    conn.close()
 
-	# The premailerData is a JSON. Convert it to a Python dict.
-	premailerDict = json.loads(premailerData)
-	urls['Premailer HTML'] = premailerDict['documents']['html']
-	urls['Premailer Text'] = premailerDict['documents']['txt']
+    # The premailerData is a JSON. Convert it to a Python dict.
+    premailerDict = json.loads(premailerData)
+    urls['Premailer HTML'] = premailerDict['documents']['html']
+    urls['Premailer Text'] = premailerDict['documents']['txt']
 
-	files['Premailer HTML'] = urllib2.urlopen(urls['Premailer HTML'])
-	files['Premailer Text'] = urllib2.urlopen(urls['Premailer Text'])
+    files['Premailer HTML'] = urllib2.urlopen(urls['Premailer HTML'])
+    files['Premailer Text'] = urllib2.urlopen(urls['Premailer Text'])
 
-	# We have to clean the CDATA tags from what we get from Premailer.
-	CDATAmsgCount = 0
-	with open(filenames['Mail'], 'w') as f:
-		withinStyle = False
-		for line in files['Premailer HTML']:
-			# Only look for CDATA tags within the <style></style> section
-			if line.strip().startswith('<style'):
-				withinStyle = True # We're in the style section
+    # We have to clean the CDATA tags from what we get from Premailer.
+    CDATAmsgCount = 0
+    with open(filenames['Mail'], 'w') as f:
+        withinStyle = False
+        for line in files['Premailer HTML']:
+            # Only look for CDATA tags within the <style></style> section
+            if line.strip().startswith('<style'):
+                withinStyle = True # We're in the style section
 
-			if '</style>' in line:
-				withinStyle = False # We're not in the style section anymore
-			
-			if (withinStyle == True):
-				# If neither of these two tags are in the line, write the line.
-				# This, in effect, "deletes" the two tags from the copy.
-				if ('<![CDATA[' not in line) & (']]>' not in line):
-					f.write(line)
-				else:
-					CDATAmsgCount = CDATAmsgCount + 1
-					if CDATAmsgCount == 1:
-						print "Removing <![CDATA[...]]>..."
+            if '</style>' in line:
+                withinStyle = False # We're not in the style section anymore
+            
+            if (withinStyle == True):
+                # If neither of these two tags are in the line, write the line.
+                # This, in effect, "deletes" the two tags from the copy.
+                if ('<![CDATA[' not in line) & (']]>' not in line):
+                    f.write(line)
+                else:
+                    CDATAmsgCount = CDATAmsgCount + 1
+                    if CDATAmsgCount == 1:
+                        print "Removing <![CDATA[...]]>..."
 
-			else:	# If we're not in the style section, write everything.
-				f.write(line) 
-	
+            else:   # If we're not in the style section, write everything.
+                f.write(line) 
+    
 else:
-	with open(filenames['Mail'], 'w') as f:
-		print "Copying original HTML file to a file for mailing..."
-		f.write(content)
+    with open(filenames['Mail'], 'w') as f:
+        print "Copying original HTML file to a file for mailing..."
+        f.write(content)
 
 # Write plain text versions
 if (settings.as_bool('use_premailer') == True) & \
-		(settings.as_bool('use_lynx_for_text') == False):
-	with open(filenames['Text'], 'w') as f:
-		print "Creating plain text version from Premailer..."
-		text = files['Premailer Text'].read()
-		f.write(text)
+        (settings.as_bool('use_lynx_for_text') == False):
+    with open(filenames['Text'], 'w') as f:
+        print "Creating plain text version from Premailer..."
+        text = files['Premailer Text'].read()
+        f.write(text)
 else:
-	optList = [
-		'-display_charset=utf-8', 
-		'-width=1024',
-		'-dump',
-		'-stdin'
-	]
-	htmlAsString = ''.join(files['Original'])
-	print "Creating plain text version from lynx..."
-	lynx = subprocess.Popen(['lynx'] + optList,
-													stdin=subprocess.PIPE,
-													stdout=subprocess.PIPE,
-													stderr=subprocess.PIPE)
-	text = lynx.communicate(htmlAsString)[0]
-	with open(filenames['Text'], 'w') as f:
-		f.write(text)
+    optList = [
+        '-display_charset=utf-8', 
+        '-width=1024',
+        '-dump',
+        '-stdin'
+    ]
+    htmlAsString = ''.join(files['Original'])
+    print "Creating plain text version from lynx..."
+    lynx = subprocess.Popen(['lynx'] + optList,
+                                                    stdin=subprocess.PIPE,
+                                                    stdout=subprocess.PIPE,
+                                                    stderr=subprocess.PIPE)
+    text = lynx.communicate(htmlAsString)[0]
+    with open(filenames['Text'], 'w') as f:
+        f.write(text)
 
 ### End Premailer section
 
 # Close the files
 
 for value in files.itervalues():
-	if isinstance(value, file): # If it's a file object, close it
-		value.close()
+    if isinstance(value, file): # If it's a file object, close it
+        value.close()
 
 filesToPublish.append(filenames['Archive'])
 
@@ -329,37 +329,37 @@ print "Publishing files..."
 sourcePath = os.getcwd()
 
 publishPath = os.path.join(settings['www-docs_root'],
-							settings['path_to_site_folder'])
+                            settings['path_to_site_folder'])
 
 if os.path.exists(publishPath) == False:
-	os.makedirs(publishPath)
+    os.makedirs(publishPath)
 
 # Walk the directories and make them web-viewable
 WebDirPerms(settings['www-docs_root'], publishPath)
 
 # Copy the files to the published directory
 for item in filesToPublish:
-	dirpath = os.path.dirname(item)
-	filename = os.path.basename(item)
-	publishDirpath = os.path.join(publishPath, dirpath)
-	# Make directories in publishPath
-	if os.path.exists(publishDirpath) == False:
-		os.makedirs(publishDirpath)
-		
-	WebDirPerms(publishPath, publishDirpath)
-	publishFilepath = os.path.join(publishDirpath, filename)
-	shutil.copy(item, publishDirpath)
-	os.chmod(publishFilepath, 0644)
+    dirpath = os.path.dirname(item)
+    filename = os.path.basename(item)
+    publishDirpath = os.path.join(publishPath, dirpath)
+    # Make directories in publishPath
+    if os.path.exists(publishDirpath) == False:
+        os.makedirs(publishDirpath)
+        
+    WebDirPerms(publishPath, publishDirpath)
+    publishFilepath = os.path.join(publishDirpath, filename)
+    shutil.copy(item, publishDirpath)
+    os.chmod(publishFilepath, 0644)
 
 print "Done."
 
 # Display URL of Archive version (for debugging)
 print '\nURL for Archive version:\n{0}\n'.format(urls['Archive'])
-		
+        
 # sys.exit(0)
 
 
-		
-		
+        
+        
 
 
